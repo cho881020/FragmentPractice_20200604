787e22e68253055641c428602222f913a181bc7b
kj_cho <kj_cho@idealidea.co.kr>
2020-06-04 21:04:00 +0900

    Initial commit

---
+++ app/src/main/java/kr/co/tjoeun/fragmentpractice_20200604/MainActivity.kt
@@ -0,0 +1,22 @@
+package kr.co.tjoeun.fragmentpractice_20200604
+
+import android.os.Bundle
+
+class MainActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
+}
