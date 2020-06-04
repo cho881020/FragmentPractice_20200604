0ac38acad5c5ff5363f216d97c4f7ffe0ab7c8e0
kj_cho <kj_cho@idealidea.co.kr>
2020-06-04 22:01:11 +0900

    빈 액티비티를 시작점으로 변경  - 뷰 페이저 작업의 시작점

---
+++ app/src/main/java/kr/co/tjoeun/fragmentpractice_20200604/MyViewPagerActivity.kt
@@ -0,0 +1,23 @@
+package kr.co.tjoeun.fragmentpractice_20200604
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MyViewPagerActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_view_pager)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
+}
